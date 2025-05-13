4cd5b3e395125f162da88ecb5fe41244b47d42c4
Kelvinm <kelvinmurithi18@gmail.com>
2025-05-13 07:31:18 +0300

    Lab done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,53 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name="Fido", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+    # name property
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # breed property
+    def get_breed(self):
+        print("Retrieving breed.")
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in Dog.APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+dog1 = Dog("Max", "Beagle")
+# Output: Setting name to Max
+#         Setting breed to Beagle
+
+dog2 = Dog("Buddy", "Dragon Dog")
+# Output: Setting name to Buddy
+#         Breed must be in list of approved breeds.
+
+print(dog1.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,47 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+]
+    def __init__(self, name="Kelvin", job="Engineer"):
+        self.name = name
+        self.job = job
+    
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if isinstance (name, str) and (1<= len(name)<=25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+    
+    # job
+    
+    def get_job(self):
+        print("Retrieving job.")
+        return self._job
+
+    def set_job(self, job):
+        if job in Person.APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+    pass
     pass
